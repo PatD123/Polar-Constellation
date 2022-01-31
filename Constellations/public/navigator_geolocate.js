e9be13482bebade427e1b338e225ddc6376236da
PatD123 <patrickdai917@gmail.com>
2022-01-30 20:48:27 -0500

    nuthin

--- Constellations/public/navigator_geolocate.js
+++
@@ -1,7 +0,0 @@
-if("geolocation" in navigator){
-    console.log("You can use geolocator!");
-    navigator.geolocation.getCurrentPosition((position) => {
-        const lat = position.coords.latitude;
-        const long = position.coords.longitude;
-    });
-}
